48c8149c602ccd5538cab75345db2f1380d90761
MohsenSami <73436220+mohsensami@users.noreply.github.com>
2022-06-09 22:44:23 +0430

    update for Contact post type

--- themes/api/functions.php
+++ themes/api/functions.php
@@ -2,90 +2,90 @@
 
 //Register post type
 //
-if( !function_exists('pricode_movies_post_type_callback') ){
-	function pricode_movies_post_type_callback() {
+if( !function_exists('sami_contact_post_type_callback') ){
+	function sami_contact_post_type_callback() {
 		$args = array(
 			'public'    => true,
-			'label'     => __( 'Movies', 'textdomain' ),
+			'label'     => __( 'Contacts', 'textdomain' ),
 			'supports' => [ 'custom-fields',  'title', 'editor' ]
 		);
-		register_post_type( 'movie', $args );
+		register_post_type( 'contact', $args );
 	}
-	add_action( 'init', 'pricode_movies_post_type_callback' );	
+	add_action( 'init', 'sami_contact_post_type_callback' );	
 }
 
 
 
 
 add_action('init', function(){
-    register_rest_route( 'pricode/v1', '/test/(?P<pricode_param>\d+)', array(
+    register_rest_route( 'api/v1', '/test/(?P<sami_param>\d+)', array(
         'methods' => 'GET',
-        'callback' => 'pricode_api_test_callback'
+        'callback' => 'sami_api_test_callback'
 
     ));
 });
 
 //GET routes
 add_action('init', function(){
-    register_rest_route( 'pricode/v1', '/movies/', array(
+    register_rest_route( 'api/v1', '/contacts/', array(
         'methods' => 'GET',
-        'callback' => 'pricode_api_get_callback'
+        'callback' => 'sami_api_get_callback'
 
     ));
 });
 add_action('init', function(){
-    register_rest_route( 'pricode/v1', '/movies/(?P<movie_id>\d+)', array(
+    register_rest_route( 'api/v1', '/contacts/(?P<contact_id>\d+)', array(
         'methods' => 'GET',
-        'callback' => 'pricode_api_get_callback'
+        'callback' => 'sami_api_get_callback'
 
     ));
 });
 
 //POST
 add_action('init', function(){
-    register_rest_route( 'pricode/v1', '/movies/create/', array(
+    register_rest_route( 'api/v1', '/contacts/create/', array(
         'methods' => 'POST',
-        'callback' => 'pricode_api_post_callback'
+        'callback' => 'sami_api_post_callback'
 
     ));
 });
 
 //PUT
 add_action('init', function(){
-    register_rest_route( 'pricode/v1', '/movies/update/(?P<movie_id>\d+)', array(
+    register_rest_route( 'api/v1', '/contacts/update/(?P<contact_id>\d+)', array(
         'methods' => 'PUT',
-        'callback' => 'pricode_api_put_callback'
+        'callback' => 'sami_api_put_callback'
 
     ));
 });
 
 //DELETE
 add_action('init', function(){
-    register_rest_route( 'pricode/v1', '/movies/(?P<movie_id>\d+)', array(
+    register_rest_route( 'api/v1', '/contacts/(?P<contact_id>\d+)', array(
         'methods' => 'DELETE',
-        'callback' => 'pricode_api_delete_callback'
+        'callback' => 'sami_api_delete_callback'
 
     ));
 });
 
-function pricode_api_test_callback( $request ){
+function sami_api_test_callback( $request ){
     $response['status'] =  200;
     $response['success'] = true;
-    $response['data'] = $request->get_param('pricode_param');
+    $response['data'] = $request->get_param('sami_param');
     return new WP_REST_Response( $response );
 }
 
 /**
- * Get all movies from our WordPress Installation
+ * Get all contacts from our WordPress Installation
  * @param  object $request WP_Request with data
  * @return obeject         WP_REST_Response
  */
-function pricode_api_get_callback( $request ){
+function sami_api_get_callback( $request ){
 
     
-    $movie_id = $request->get_param('movie_id');
-    if( empty( $movie_id ) ){
-    	$posts = get_posts( [ 'post_type' => 'movie', 'post_status' => 'publish' ] );
+    $contact_id = $request->get_param('contact_id');
+    if( empty( $contact_id ) ){
+    	$posts = get_posts( [ 'post_type' => 'contact', 'post_status' => 'publish' ] );
 	   
 	    if( count($posts) > 0 ){
 	    	$response['status'] =  200;	
@@ -97,8 +97,8 @@ function pricode_api_get_callback( $request ){
 	    	$response['message'] = 'NO posts!';
 	    }
     }else{
-    	if( $movie_id > 0 ){
-    		$post = get_post( $movie_id );	
+    	if( $contact_id > 0 ){
+    		$post = get_post( $contact_id );	
     		if( !empty( $post ) ){
     			$response['status'] =  200;	
 		    	$response['success'] = true;
@@ -117,11 +117,11 @@ function pricode_api_get_callback( $request ){
 }
 
 /**
- * Create a movie post by rest api
+ * Create a contact post by rest api
  * @param  object $request WP_Request with data
  * @return obeject         WP_REST_Response
  */
-function pricode_api_post_callback( $request ){
+function sami_api_post_callback( $request ){
 
 
 	$post['post_title'] = sanitize_text_field( $request->get_param( 'title' ) );
@@ -130,7 +130,7 @@ function pricode_api_post_callback( $request ){
 		'genre' => sanitize_text_field( $request->get_param( 'meta_genre' ) )
 	];
 	$post['post_status'] = 'publish';
-	$post['post_type'] = 'movie';
+	$post['post_type'] = 'contact';
 	$new_post_id = wp_insert_post( $post );
 
 	if( !is_wp_error( $new_post_id ) ){
@@ -149,21 +149,21 @@ function pricode_api_post_callback( $request ){
 
 
 /**
- * Update a movie post
+ * Update a contact post
  * @param  object $request WP_Request with data
  * @return obeject         WP_REST_Response
  */
-function pricode_api_put_callback( $request ){
-	$movie_id = $request->get_param('movie_id');
-	if( $movie_id > 0 ){
-		$post['ID'] = $movie_id;
+function sami_api_put_callback( $request ){
+	$contact_id = $request->get_param('contact_id');
+	if( $contact_id > 0 ){
+		$post['ID'] = $contact_id;
 		$post['post_title'] = sanitize_text_field( $request->get_param( 'title' ) );
 		$post['post_content'] = sanitize_text_field( $request->get_param( 'content' ) );
 		$post['meta_input'] = [
 			'genre' => sanitize_text_field( $request->get_param( 'meta_genre' ) )
 		];
 		$post['post_status'] = 'publish';
-		$post['post_type'] = 'movie';
+		$post['post_type'] = 'contact';
 		$new_post_id = wp_update_post( $post, true );
 
 		if( !is_wp_error( $new_post_id ) ){
@@ -180,15 +180,15 @@ function pricode_api_put_callback( $request ){
 	}else{
 		$response['status'] =  200;	
 		$response['success'] = false;
-		$response['message'] = 'Movie id is no set!';	
+		$response['message'] = 'Contact id is no set!';	
 	}
 	return new WP_REST_Response( $response );
 }
 
-function pricode_api_delete_callback( $request ){
-	$movie_id = $request->get_param('movie_id');
-	if( $movie_id > 0 ){
-		$deleted_post = wp_delete_post( $movie_id );
+function sami_api_delete_callback( $request ){
+	$contact_id = $request->get_param('contact_id');
+	if( $contact_id > 0 ){
+		$deleted_post = wp_delete_post( $contact_id );
 		if( !empty( $deleted_post ) ){
 			$response['status'] =  200;	
 			$response['success'] = true;
@@ -201,7 +201,7 @@ function pricode_api_delete_callback( $request ){
 	}else{
 		$response['status'] =  200;	
 		$response['success'] = false;
-		$response['message'] = 'Movie id is no set!';	
+		$response['message'] = 'Contact id is no set!';	
 	}
 	return new WP_REST_Response( $response );	
 }
