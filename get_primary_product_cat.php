5c2032fef423f49fbb6c6a3e990e3ad4348b5019
MohsenSami <73436220+mohsensami@users.noreply.github.com>
2022-10-09 11:12:55 +0330

    Create get_primary_product_cat.php

---
+++ get_primary_product_cat.php
@@ -0,0 +1,40 @@
+<?php
+
+function get_post_primary_category($post_id, $term='category', $return_all_categories=false){
+    $return = array();
+
+    if (class_exists('WPSEO_Primary_Term')){
+        // Show Primary category by Yoast if it is enabled & set
+        $wpseo_primary_term = new WPSEO_Primary_Term( $term, $post_id );
+        $primary_term = get_term($wpseo_primary_term->get_primary_term());
+
+        if (!is_wp_error($primary_term)){
+            $return['primary_category'] = $primary_term;
+        }
+    }
+
+    if (empty($return['primary_category']) || $return_all_categories){
+        $categories_list = get_the_terms($post_id, $term);
+
+        if (empty($return['primary_category']) && !empty($categories_list)){
+            $return['primary_category'] = $categories_list[0];  //get the first category
+        }
+        if ($return_all_categories){
+            $return['all_categories'] = array();
+
+            if (!empty($categories_list)){
+                foreach($categories_list as &$category){
+                    $return['all_categories'][] = $category->term_id;
+                }
+            }
+        }
+    }
+
+    return $return;
+}
+
+
+
+$post_categories = get_post_primary_category($post->ID, 'product_cat');
+$primary_category = $post_categories['primary_category'];
+var_dump($primary_category->term_id);
